3062042749e3f883327ef3a5a3ce74f896b6c1ee
windinjuries <294394591@qq.com>
2023-03-14 08:52:32 +0000

    update

--- main.c
+++ main.c
@@ -1,5 +1,20 @@
 #include "stdio.h"
+typedef struct {
+    int a;
+    int b;
+} stu;
+struct student {
+    int c;
+};
 int main() 
 {
-    printf("hello\n");
+    
+    stu s;
+    s.a=12;
+    printf("%d",s.a);
+    struct student s2;
+    s2.c =12;
+    printf("%d",s2.c);
+    printf("%d",printf("123"));
+
 }
