cb689a5aec8bca29faa72df96ca1d9ebe62f0614
Wind <34563105+windinjuries@users.noreply.github.com>
2023-03-22 10:07:38 +0800

    [2023.3.22 10:07] add file

--- main.c
+++ main.c
@@ -1,17 +1,6 @@
-#include "stdio.h"
-typedef struct {
-    int a;
-    int b;
-} stu;
-struct student {
-    int c;
-};
+#include "inc/main.h"
+
 int main() 
 {
-    stu s;
-    s.a=12;
-    printf("%d",s.a);
-    struct student s2;
-    s2.c =12;
-    printf("%d",s2.c);
+    printf("hello");
 }
