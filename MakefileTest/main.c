18babfa77a42e04f7e536999188d77945fbfe55f
windinjuries <34563105+windinjuries@users.noreply.github.com>
2023-04-25 15:11:14 +0800

    [2023.4.25 15:09]

--- MakefileTest/main.c
+++ MakefileTest/main.c
@@ -1,6 +1,25 @@
 #include "inc/main.h"
 #include "inc/sub.h"
-int main() 
+#include <pthread.h>
+void *thread1();
+int main(int argc, char *argv[], char *envp[])
 {
-    printf("%d",a);
+    pthread_t pid;
+    int error = pthread_create(&pid, NULL, &thread1, NULL);
+    if (error < 0)
+        return -1;
+    while(1);
+}
+void *thread1()
+{
+    int i = 0;
+    while (1)
+    {
+        if (i > 100)
+            i = 0;
+        else
+            i++;
+        sleep(100);
+        printf("i=%d\n", i);
+    }
 }
