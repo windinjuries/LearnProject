d60a57c8e18fae03d114c7ca78055993cf3d88ec
windinjuries <34563105+windinjuries@users.noreply.github.com>
2023-04-28 19:22:48 +0800

    [2023.4.28 19:22]

--- MakefileTest/main.c
+++ MakefileTest/main.c
@@ -1,25 +1,53 @@
 #include "inc/main.h"
 #include "inc/sub.h"
 #include <pthread.h>
+#include <bits/pthreadtypes.h>
+#include <syslog.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <stdlib.h>
+typedef struct 
+{
+    int num;
+    pthread_mutex_t f_lock;
+} st;
+st st1;
 void *thread1();
+void *thread2();
+
 int main(int argc, char *argv[], char *envp[])
 {
-    pthread_t pid;
-    int error = pthread_create(&pid, NULL, &thread1, NULL);
+    pthread_t pid1;
+    pthread_t pid2;
+    int error;
+    error = pthread_mutex_init(&st1.f_lock, NULL);
     if (error < 0)
         return -1;
-    while(1);
+    error = pthread_create(&pid1, NULL, &thread1, NULL);
+    if (error < 0)
+        return -1;
+    printf("%ld",pid1);
+    error = pthread_create(&pid2, NULL, &thread1, NULL);
+    if (error < 0)
+        return -1;
+    printf("%ld",pid2);
+    while (1)
+    {
+        ;/* code */
+    } 
 }
 void *thread1()
 {
-    int i = 0;
+    pthread_t tid = gettid();
     while (1)
     {
-        if (i > 100)
-            i = 0;
+        pthread_mutex_trylock(&(st1.f_lock));
+        if (st1.num > 100)
+            st1.num = 0;
         else
-            i++;
-        sleep(100);
-        printf("i=%d\n", i);
+            st1.num++;
+        printf("%d: i=%d\n", tid, st1.num);
+        pthread_mutex_unlock(&(st1.f_lock));
+        sleep(2); 
     }
 }
