af83effd6f790fac5c5de0d56cac3f77aa3dde73
windinjuries <294394591@qq.com>
2024-01-01 10:00:09 +0800

    fix:delete MyProject and Project

--- MyProject/README.md
+++
@@ -1,7 +0,0 @@
-## My Personal Project
-## build tool
-- xmake + aarch64-linux-gcc
-
-### log 
-- [log.c](www.github.com/rxi/log.c.git)
-
--- MyProject/main/main.c
+++
@@ -1,7 +0,0 @@
-#include "log.h"
-#include <stdio.h>
-int main(int argc, char *argv[])
-{ 
-    log_info("TEST", "123\n");
-    return 0;
-}
